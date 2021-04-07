80ab46278a83dc8b03150a2c7a54e8f77930c35d
MariaCherkez <33730446+MariaCherkez@users.noreply.github.com>
2021-04-07 21:39:15 +0300

    feat: made delete-digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,15 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let isChange = false;
+  return parseInt(String(n).split('').filter((e, i, a) => {
+    if (!isChange && parseInt(e, 10) === Math.min.apply(null, a)) {
+      isChange = true;
+      return false;
+    }
+    return true;
+  }).join(''), 10);
 }
 
 module.exports = deleteDigit;
